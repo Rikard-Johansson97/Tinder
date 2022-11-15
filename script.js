1d9ee1f6f712d9896db1214dd65edf4748776b35
Rikard Johansson <Rikard.com@hotmail.com>
2022-11-15 19:42:42 +0100

    optimized and cleaner code

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-const imgContainer = document.querySelector(".img-container");
-const matchesContainer = document.querySelector(".matches-container");
-
-const matches = [];
-
-//
-const displayMatch = document.querySelector(".you-match");
+const imgContainer = document.querySelector(".img-container"); // img card
+const matchesContainer = document.querySelector(".matches-container"); // IF match place them here
 document.body.addEventListener("click", (e) => {
     switch (e.target.id) {
         case "like":
-            checkIfMatch();
+            getUserData(true);
             break;
         case "dislike":
-            callUserData(false);
+            getUserData(false);
             break;
         case "show-info":
             const showInfo = document.querySelector(".show-more-info");
@@ -25,130 +20,86 @@ document.body.addEventListener("click", (e) => {
             }
             break;
         default:
-            console.log("return");
             break;
     }
 });
+let activeCard = []; // Active card stores the user we want to check if we match with.
 
-// get data from api https://randomuser.me/api/
-const getUserData = async () => {
-    const response = await fetch("https://randomuser.me/api/");
-    if (response.status !== 200) {
-        throw new Error();
-    }
+const getUserData = async (like) => {
+    const response = await fetch("https://randomuser.me/api/"); // här hämtas datan från en server
+    if (response.status !== 200) throw new Error("respons status is not 200"); // testar om serverns respons status inte är 200
     const userData = await response.json();
-    return userData.results[0];
-};
-
-const checkIfMatch = () => {
-    const firstRnd = Math.floor(Math.random() * 2);
-    const secondRnd = Math.floor(Math.random() * 2);
-    if (firstRnd === secondRnd) {
+    if (like && activeCard.gender === "female") {
         displayMatchMessage();
-        setTimeout(callUserData, 1500, true);
-    } else callUserData(false);
+        displayMatch();
+        setTimeout(() => {
+            activeCard = userData.results[0];
+            return displayUserData(userData.results[0]);
+        }, 500);
+    } else {
+        activeCard = userData.results[0];
+        return displayUserData(userData.results[0]);
+    }
 };
-
-const callUserData = (status) => {
-    getUserData(status)
-        .then((userData) => {
-            console.log("resolved", userData);
-            displayUserData(
-                status,
-                userData.name.first,
-                userData.dob.age,
-                userData.location.city,
-                userData.picture.large,
-                getDistanceFromLatLonInKm(
-                    userData.location.coordinates.latitude,
-                    userData.location.coordinates.longitude
-                ),
-                userData.cell,
-                userData.gender
-            );
-        })
-        .catch((err) => {
-            console.log("rejected", err);
-        });
+const displayMatch = () => {
+    matchesContainer.innerHTML += `<div class="match">
+        <img
+            src="${activeCard.picture.large}"
+            alt="user"
+            class="match-img" />
+        <p class="match-name">${activeCard.name.first} <span class="match-age">${activeCard.dob.age}</span></p>
+    </div> -->`;
+};
+const displayMatchMessage = () => {
+    const matchOrNot = document.querySelector(".you-match");
+    matchOrNot.style.fontSize = "60px";
 };
 // MY LATITUDE: 57.107117 MY LONGITUDE: 12.252091
 function getDistanceFromLatLonInKm(dateLat, dateLong) {
-    var radiusOfEarth = 6378; // Radius of the earth in km
-    var dLat = deg2rad(dateLat - 57.107117); // deg2rad below
-    var dLon = deg2rad(dateLong - 12.252091);
-    var a =
+    let radiusOfEarth = 6378; // Radius of the earth in km
+    let dLat = deg2rad(dateLat - 57.107117); // deg2rad below
+    let dLon = deg2rad(dateLong - 12.252091);
+    let a =
         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
         Math.cos(deg2rad(57.107117)) *
             Math.cos(deg2rad(dateLat)) *
             Math.sin(dLon / 2) *
             Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = radiusOfEarth * c; // Distance in km
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let d = radiusOfEarth * c; // Distance in km
     return Math.floor(d);
 }
-function deg2rad(deg) {
-    return deg * (Math.PI / 180);
-}
-
-const displayUserData = (
-    status,
-    name,
-    age,
-    home,
-    img,
-    location,
-    number,
-    gender
-) => {
-    matches.push({ name, age, img });
-    console.log(matches);
-    if (status) displayMatches();
-
+const deg2rad = (deg) => deg * (Math.PI / 180);
+const displayUserData = (userData) => {
     imgContainer.innerHTML = `<img
-    src="${img}"
+    src="${userData.picture.large}"
     alt="user"
     class="date-img" />
 <div class="date-info-container">
-    <h2 class="name">${name}<span class="age"> ${age}</span></h2>
+    <h2 class="name">${userData.name.first}<span class="age"> ${
+        userData.dob.age
+    }</span></h2>
     <p>
         <i class="fa-solid fa-house"></i>
-        <span class="home">${home}</span>
+        <span class="home">${userData.location.city}</span>
     </p>
     <p>
         <i class="fa-solid fa-location-dot"></i>
-        <span class="location">${location} KM</span>
+        <span class="location">${getDistanceFromLatLonInKm(
+            userData.location.coordinates.latitude,
+            userData.location.coordinates.longitude
+        )} KM</span>
     </p>
     <div class="show-more-info">
-        <p><i class="fa-solid fa-phone"></i><a class="tel" href=“tel:${number}">${number}</a></p>
-        <p><i class="fa-solid fa-mars-and-venus"></i><span>${gender}</span></p>
+        <p><i class="fa-solid fa-phone"></i><a class="tel" href=“tel:${
+            userData.cell
+        }">${userData.cell}</a></p>
+        <p><i class="fa-solid fa-mars-and-venus"></i><span>${
+            userData.gender
+        }</span></p>
     </div>
 </div>
 <h4 class="you-match true">MATCH</h4>
 <i id="show-info" class="fa-solid fa-arrow-up"></i>`;
 };
-
-const displayMatches = () => {
-    // HJÄLP HÄR! varför måste jag ha matches.length - 1 på första sen byta till -2
-    let i = matches.length - 1;
-    if (matches.length > 1) {
-        i = matches.length - 2;
-    } else {
-        i = matches.length - 1;
-    }
-    matchesContainer.innerHTML += `<div class="match">
-        <img
-            src="${matches[i].img}"
-            alt="user"
-            class="match-img" />
-
-        <p class="match-name">${matches[i].name} <span class="match-age">${matches[i].age}</span></p>
-    </div> -->`;
-};
-
-const displayMatchMessage = () => {
-    const matchOrNot = document.querySelector(".you-match");
-    console.log(matchOrNot);
-    matchOrNot.style.fontSize = "60px";
-};
-
-callUserData(false);
+getUserData(false);
